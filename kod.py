e0550fc13eea358dd060337110f2ca389dc2a910
aslanzov <rus.abazov.99@mail.ru>
2021-02-27 13:48:00 +0300

    new

--- kod.py
+++ kod.py
@@ -2,13 +2,22 @@
 str_input = input("Delimoe: ")
 delimoe = int(str_input)
 #print(type(delimoe))
-peration = input ("+ / * - ^ :")
+operation = input ("+ / * - ^ :")
 str_input2 = input("Delitel: ")
 delitel = int(str_input2)
 #print(type(delitel))
+result = None
 
 if operation == '/':
-   result = delimoe/delitel
+    result = delimoe / delitel
+elif operation == '+':
+    result = delimoe + delitel
+elif operation == '*':
+    result = delimoe * delitel
+elif operation == '^':
+    result = delimoe**delitel
+else:
+    result = "unknown"
 
 #result = delimoe / delitel
 #print(type(result))
