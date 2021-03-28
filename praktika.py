ad98ab1786bb883657a3431cc4d85891eb063bd5
aslanzov <rus.abazov.99@mail.ru>
2021-03-28 21:21:09 +0300

    Calc with priority

--- praktika.py
+++ praktika.py
@@ -15,7 +15,7 @@ instr = instr.replace(' ', '')
 на выход ?структура данных? с операциями +- и значениями
 (+*-)
 (-2 3.5 2 3 2)
-[{'opr' :  , 'val': -2}, {}, ...]
+[{'opr' : '' , 'val': -2}, {}, ...]
 """
 
 hp_ops = tuple('^')
@@ -30,27 +30,26 @@ d = dict()
 d['opr']='First'
 d['val']= ''
 actions.append(d)
-actions.append(d)
 
 result = None
-
-#премся по строчке, карент -- текущее, делим на операции и числа
+error = False
+#премся по строчке, карент -- текущее, парсим на операции и числа
 #пока не понимаю, какой алгоритм с отрицательными числами
-cur = ''
-i=0
+#cur = ''
+#i=0
 for i, letter in enumerate(instr): 
-    if letter in supported_ops and (i>0) and cur!='': 
-        actions[-1]['val'] = float (cur)
-        cur = ''
-        actions.append ({'opr': letter, 'val': ''})
+    if letter in supported_ops and (i>0) and actions[-1]['val'] != '':
+        #cur = ''
+        #операции
+        actions.append({'opr': letter, 'val': ''})
     
     elif letter in digital_chars:
-        cur+=letter
+        #cur+=letter
+        #чиселки
         actions [-1]['val'] += letter
  
 
-print (actions)
-
+#print (actions)
 
 #вычислить операции 1го приоритета (возведение в степень)
 """
@@ -60,17 +59,20 @@ print (actions)
 2+3.5*2-9
 """
 
-
 i = 0
+actions.reverse()
 while i < len(actions):
-	do = actions[i]
-	operation = do.get('opr')
-	if operation == hp_ops[0]:
-		actions[i+1]['val'] = actions[i+1].get('val') ** do.get('val')
-		actions.remove(do)
-	else:
-		i += 1
-   # pass
+    do = actions[i]
+    operation = do.get('opr')
+    if operation in hp_ops:
+        if operation == '^':
+            cur_res = float(actions[i+1].get('val'))**float(do.get('val'))
+            actions[i+1]['val'] = str(cur_res)
+            del actions[i]
+    else:
+        i += 1
+# pass
+actions.reverse()
 
 
 
@@ -80,12 +82,40 @@ while i < len(actions):
 на выход обновленная ?структура данных? с операциями +- и значениями
 -2+3.5*2-9
 2+7-9
+
+сначала чекаем ошибку деления, если всё ок, то работаем дальше
 """
 
-  
+i = 0
+while i < len(actions):
+    do = actions[i]
+    operation = do.get('opr')
+    if operation in mp_ops:
+        if float(do.get('val')) == 0 and operation == '/':
+            result = 'inf'
+            error = True
+        else:
+            eval_str = (actions[i-1].get('val') + operation + do.get('val'))
+            cur_res = eval(eval_str)
+            actions[i-1]['val'] = str(cur_res)
+        actions.pop(i)
+    else:
+        i += 1
 
 #вычислить операции 3го приоритета (сложение и вычитание)
-# -2+7-9 = -4
+""" 
+Если выше все отработало, то выполняем +-
+-2+7-9 = -4
+"""
+if not error:
+    for do in actions:
+       chislo_A = result
+       chislo_B = do.get('val')
+       operation = do.get('opr')
+       if operation in lp_ops:
+          result = str(eval(chislo_A + operation + chislo_B))
+       else:
+          result = chislo_B
 
 #вывести результат
 print("Result: " + str(result))
